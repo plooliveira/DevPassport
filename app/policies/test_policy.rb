d7d770fe2bab0bcb9593dbed2b73af3c6fb59bc8
Allan Paladino <allanrp@gmail.com>
2020-11-21 15:36:03 -0300

    sucesso!

--- app/policies/test_policy.rb
+++ app/policies/test_policy.rb
@@ -1,43 +1,16 @@
 class TestPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      # scope => Test
-      scope.all
-    end
-
-    def index?
-    end
-
-    def new?
-      true
-    end
-
-    def create?
-    end
-
-    def edit?
-      raise
-
-      record.user.role == 3
-    end
-
-    def update?
-    end
-
-    def destroy?
-      is_company?
-    end
-
-    private
-
-    def is_company?
-      # user => current_user
-      # record => @test
-      # record.user.role == 3
+      scope.where(user: user)
     end
   end
 
   def index?
     true
   end
+
+  def edit?
+    record.user == user
+  end
+
 end
