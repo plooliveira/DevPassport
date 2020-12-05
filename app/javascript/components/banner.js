c622f4aaffd26af6ea5a2cb0d26fcb33895e4669
Walter Jr <wj@tomcix.com>
2020-12-05 00:22:11 -0300

    Home finalized and login form adjusted

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,8 +3,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["The best way to be approved in the main technologies companies of the world!"],
-    typeSpeed: 50,
+    strings: ["The best way to be approved in the most of technologies companies of the world!", "Accept the Challenge!", "Be Protagonist!", "Reach the Top!", "Lead the Future!"],
+    typeSpeed: 80,
     loop: true
   });
 }
