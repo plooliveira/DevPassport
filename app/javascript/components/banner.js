ea9a1128ee22f3fab61cff986a285a0c7f247a0c
Allan Paladino <allanrp@gmail.com>
2020-12-05 15:04:18 -0300

    fixes

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,9 +4,10 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Imagine a world where you can accelerate your company's innovation!", "We brought together software developers, hiring managers and recruiting professionals to innovate the hiring process", "Lead the Future!", "Accept the challenge!"],
-    typeSpeed: 80,
+    typeSpeed: 50,
     loop: true
   });
 }
 
 export { loadDynamicBannerText };
+
