4dde609e182986f75f250579dc5c608a4af52f3c
Allan Paladino <allanrp@gmail.com>
2020-12-05 14:53:50 -0300

    js changed

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Imagine a world where you can accelerate your company's innovation!", "We brought together software developers, hiring managers and recruiting professionals from leading companies to innovate the hiring processes world!", "Lead the Future!", "Accept the challenge!"],
+    strings: ["Imagine a world where you can accelerate your company's innovation!", "We brought together software developers, hiring managers and recruiting professionals to innovate the hiring process", "Lead the Future!", "Accept the challenge!"],
     typeSpeed: 80,
     loop: true
   });
