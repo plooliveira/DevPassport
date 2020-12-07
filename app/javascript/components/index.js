0b9cef9056bab5acde6e62994a938c5dbe88591c
Walter Jr <wj@tomcix.com>
2020-12-07 10:56:56 -0300

    Add index js to be able to click the image

---
+++ app/javascript/components/index.js
@@ -0,0 +1,17 @@
+// TODO: write your code here!
+
+// 1. Selecionar o elemento
+// 2. selecionar o evento
+// 3. o código que vai ser executado
+// 4. juntar tudo num eventListener
+const clickableButton = document.querySelectorAll(".clickable");
+
+const changeClass = (event) => {
+  event.currentTarget.classList.toggle("active");
+};
+
+const addEvent = (button) => {
+  button.addEventListener('click', changeClass);
+};
+
+clickableButton.forEach(addEvent);
